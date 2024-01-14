901f963dc9276e75b26b7aaad5c6dcf5b982e9a2
Shawn Yang <yangshawn14@gmail.com>
2024-01-13 18:47:28 -0700

    Update lesson 1

--- controllers/lesson1.js
+++ controllers/lesson1.js
@@ -6,7 +6,12 @@ const shawnRoute = (req, res) => {
     res.send("Shawn Yang");
 };
 
+const troyRoute = (req, res) => {
+    res.send('Troy Yang');
+};
+
 module.exports = {
     julieRoute,
-    shawnRoute
+    shawnRoute,
+    troyRoute
 };
