128d383e46b4d08bb3d3d561514756eca8bb6f7a
Shawn Yang <yangshawn14@gmail.com>
2024-01-19 20:40:16 -0700

    Rename file and update package.json

--- lesson02/cse341-team-lesson02/backend/server.js
+++ lesson02/cse341-team-lesson02/backend/server.js
@@ -3,3 +3,6 @@ const express = require('express');
 const port = process.env.PORT || 8080;
 const app = express();
 
+app.listen(port, () => {
+    console.log(`Server running on port localhost:${port}`)
+})
