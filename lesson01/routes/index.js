ba8b1299fe756ddf74cfa30b136ce10b0eb0266f
Shawn Yang <yangshawn14@gmail.com>
2024-01-20 16:55:41 -0700

    Delete lesson01 directory

--- lesson01/routes/index.js
+++
@@ -1,9 +0,0 @@
-const routes = require('express').Router();
-const lesson1Controller = require('../controllers/lesson1');
- 
-routes.get('/', lesson1Controller.julieRoute);
-routes.get('/shawn', lesson1Controller.shawnRoute);
-routes.get('/troy', lesson1Controller.troyRoute);
-
-module.exports = routes;
-
