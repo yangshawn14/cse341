a6261ea7ae05f1c53fe71cdd838a4031310a1458
Shawn Yang <yangshawn14@gmail.com>
2024-01-25 00:14:43 -0700

    Create rest file

--- lesson02-personal/controllers/contacts.js
+++ lesson02-personal/controllers/contacts.js
@@ -9,13 +9,31 @@ const getAll = async (req, res, next) => {
   });
 };
 
+// const getSingle = async (req, res, next) => {
+//   const userId = new ObjectId(req.params.id);
+//   const result = await mongodb.getDb().db().collection('contacts').find({ _id: userId });
+//   result.toArray().then((lists) => {
+//     res.setHeader('Content-Type', 'application/json');
+//     res.status(200).json(lists[0]);
+//   });
+// };
+
 const getSingle = async (req, res, next) => {
-  const userId = new ObjectId(req.params.id);
-  const result = await mongodb.getDb().db().collection('contacts').find({ _id: userId });
-  result.toArray().then((lists) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(lists[0]);
-  });
+  const idString = req.params.id;
+
+  try {
+    const userId = new ObjectId(idString);
+    console.log('Converted ObjectId:', userId);
+
+    const result = await mongodb.getDb().db().collection('contacts').find({ _id: userId });
+    result.toArray().then((lists) => {
+      res.setHeader('Content-Type', 'application/json');
+      res.status(200).json(lists[0]);
+    });
+  } catch (error) {
+    console.error('Error converting ObjectId:', error.message);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 };
 
 module.exports = { getAll, getSingle };
