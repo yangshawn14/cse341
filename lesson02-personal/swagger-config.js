9c2c04d341a43f38816fee53c948db91838c8e90
Shawn Yang <yangshawn14@gmail.com>
2024-02-03 22:05:14 -0700

    Change swagger 'host' url

--- lesson02-personal/swagger-config.js
+++ lesson02-personal/swagger-config.js
@@ -1,11 +1,11 @@
 const doc = {
-  info: {
-    title: 'Contacts API',
-    description: 'This is my Contacts API',
-    version: '1.0.0',
-  },
-  host: 'localhost:8080',
-  schemes: ['http'],
+    info: {
+        title: 'Contacts API',
+        description: 'This is my Contacts API',
+        version: '1.0.0',
+    },
+    host: 'https://connect-mongodb.onrender.com',
+    schemes: ['http'],
 };
 
 const outputFile = './swagger.json';
