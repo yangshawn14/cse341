88d323727eafc0b5400a4a086f292faae0ace16f
Shawn Yang <yangshawn14@gmail.com>
2024-02-03 22:41:33 -0700

    Should connect to React project

--- lesson02-personal/server.js
+++ lesson02-personal/server.js
@@ -13,6 +13,12 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(bodyParser.json());
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+  );
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
   next();
 });
 
